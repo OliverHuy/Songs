7c4ea442e8ce0641fb436a5022283a30ef34cd05
OliverHuy <huynguyen.tstc@gmail.com>
2016-10-03 22:17:03 -0500

    index user back

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -1,5 +1,5 @@
 class Singer < ApplicationRecord
-	
+	belongs_to :user
 
 	has_attached_file :image, styles: { large: "300x400#", medium: "250x300#"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
